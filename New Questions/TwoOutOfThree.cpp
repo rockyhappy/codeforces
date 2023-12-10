f705c668bd1e23a2d28b7f1abbd2cc826ca0446f
rockyhappy <115190222+rockyhappy@users.noreply.github.com>
2023-12-11 01:27:37 +0530

    TwoOutOfThree

--- New Questions/TwoOutOfThree.cpp
+++ New Questions/TwoOutOfThree.cpp
@@ -1,3 +1,8 @@
+/**
+ * This is a codeforces question and the solution for the same
+ * the question link for the same is 
+ * 
+*/
 #include<iostream>
 #include<map>
 #include<vector>
@@ -16,19 +21,69 @@ void solve()
     for (int i = 0; i < n; i++)
     {
         cin>>vec[i];
-        mp[vec[i]]++
+        mp[vec[i]]++;
     }
+    int cnt=0;
+    for(auto i: mp)
+    {
+        if(i.second>=2)cnt++;
+    }
+    if(cnt<2)
+    {
+        cout<<-1<<endl;
+        return;
+    }
+    vector<int> ans(n,1);
+    int flag1=0;
+    for(auto i : mp)
+    {
+        if(i.second>=2&& flag1==0)
+        {
+            flag1=1;
+            int flag2=0;
+            for (int j = 0; j < n; j++)
+            {
+                if(vec[j]==i.first &&flag2==1)
+                {
+                    ans[j]=2;
+                }
+                if(vec[j]==i.first && flag2==0)
+                {
+                    ans[j]=1;
+                    flag2=1;
+                }
+            }
+            
+        }
+        else if(i.second>=2&& flag1==1)
+        {
+            int flag2=0;
+            for (int j = 0; j < n; j++)
+            {
+                if(vec[j]==i.first  && flag2==1)
+                {
+                    ans[j]=3;
+                }
+                if(vec[j]==i.first && flag2==0)      
+                {
+                    ans[j]=1;
+                    flag2=1;
+                }          
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    
+    
+    cout<<endl;
     
 }
 int main()
 {
-    #ifndef ONLINE_JUDGE
-        // for getting input from input.txt
-        freopen("input.txt", "r", stdin);
-        // for writing output to output.txt
-        freopen("output.txt", "w", stdout);
-        #endif
-        fastio();
+    fastio();
     int t ;
     cin>>t;
     while(t--)
